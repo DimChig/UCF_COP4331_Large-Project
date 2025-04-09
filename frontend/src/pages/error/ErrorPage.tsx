176a6cc9f7a43207279d6bf3962458c787d0698f
DimChig <dima.chigarev@gmail.com>
2025-04-09 19:24:17 -0400

    error page

--- frontend/src/pages/error/ErrorPage.tsx
+++ frontend/src/pages/error/ErrorPage.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="w-full h-screen flex justify-center items-center p-0">
+      <h1 className="text-3xl">404 - Page Not Found</h1>
+    </div>
+  );
 };
 
 export default ErrorPage;
