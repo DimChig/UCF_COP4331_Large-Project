e1247550e2d3bdc273caa5a3156ad5b7860064d4
DimChig <dima.chigarev@gmail.com>
2025-04-09 13:02:45 -0400

    center login page

--- frontend/src/pages/login/LoginPage.tsx
+++ frontend/src/pages/login/LoginPage.tsx
@@ -1,19 +1,20 @@
-import {Card, CardHeader, CardTitle, CardContent} from "@/components/ui/card";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 
 import LoginForm from "@/components/LoginForm";
 
 const LoginPage = () => {
   return (
-    // Holy shit, this wont horizontally center for some reason. Its too late rn to think...
-    <Card className="flex w-1/3 h-min justify-center">
-      <CardHeader>
-        <CardTitle>Login to your account</CardTitle>
-      </CardHeader>
-      <CardContent>
-        {/* TODO: Actually pass the parameters and shit here. */}
-        <LoginForm />
-      </CardContent>
-    </Card>
+    <div className="flex w-full h-full items-center justify-center">
+      <Card className="flex w-1/3">
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+        </CardHeader>
+        <CardContent>
+          {/* TODO: Actually pass the parameters and shit here. */}
+          <LoginForm />
+        </CardContent>
+      </Card>
+    </div>
   );
 };
 
