610677caeb32571b6b69e758ccee50a26b5aff4c
Water-Script <lancemitchj00@gmail.com>
2025-04-09 16:44:39 -0400

    Update login card
    - Add regex password pattern matching to require passwords be at least 4 characters long, have upper and lower case letters, at least one number, and at least one special character.
    - Change username to email.
    - Add email string validation to form field.
    - Add link to go to account registration

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -8,25 +8,40 @@ import {
   FormField,
   FormItem,
   FormLabel,
+  FormMessage
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 const formSchema = z.object({
-  username: z.string(),
-  password: z.string().min(4, {
-    message: "Your password should be at least four characters"
-  })
+  email: z.string().email({ message: "Please enter a valid email." }),
+  password: z.string()
+    .min(4, { message: "Your password should be at least four characters." })
+    .regex(
+      // This regex puts a match to invalid passwords in a non-capturing group,
+      // which lets us only capture *valid* passwords.
+      /(?:.{0,4}|[^0-9]*|[^A-Z]*|[^a-z]*|[a-zA-Z0-9]*)$/g,
+      {
+        message: "Passwords need to have one lowercase letter, one uppercase letter, a number, and "
+          + "a special character."
+      }
+    )
 });
 
 const LoginForm = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    // TODO: Maybe have default values or something.
+    // Since this wraps a react-hook-form, we have to give it defaults.
+    defaultValues: {
+      email: "",
+      password: ""
+    }
   });
 
   const onSubmit = (values: z.infer<typeof formSchema>) => {
-
+    // Now we have type-safe and verified access to correct data.
+    console.log(values);  //! Remove me after debugging.
   };
 
   return (
@@ -34,17 +49,36 @@ const LoginForm = () => {
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
-          name="username"
+          name="email"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel>Email</FormLabel>
               <FormControl>
-                <Input placeholder="username" {...field} />
+                <Input placeholder="example@email.com" {...field} />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Login</Button>
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex flex-col w-full items-center justify-center space-y-2">
+          <Button type="submit">Login</Button>
+          <Link to="/register" className="underline">
+            Dont have an account? Create one here!
+          </Link>
+        </div>
       </form>
     </Form>
   );
