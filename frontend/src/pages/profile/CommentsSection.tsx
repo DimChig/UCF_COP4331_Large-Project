977e6d65abde5165f628d740c1629a70a0892a53
DimChig <dima.chigarev@gmail.com>
2025-04-11 00:04:19 -0400

    comments section init

--- frontend/src/pages/profile/CommentsSection.tsx
+++ frontend/src/pages/profile/CommentsSection.tsx
@@ -1,5 +1,14 @@
 const CommentsSection = () => {
-  return <div>CommentsSection</div>;
+  return (
+    <div className="flex flex-col w-full">
+      <div className="flex gap-2">
+        <div className="text-2xl font-semibold">Recent comments</div>
+      </div>
+      <div className="flex w-full h-full gap-4 pt-4">
+        <div className="flex w-full h-fit">container</div>
+      </div>
+    </div>
+  );
 };
 
 export default CommentsSection;
