2b770d5df7ec9593996ea2f6e31e453f930c1195
DimChig <dima.chigarev@gmail.com>
2025-04-11 00:45:51 -0400

    ucf colors

--- frontend/src/components/navbar/NavBarLogo.tsx
+++ frontend/src/components/navbar/NavBarLogo.tsx
@@ -1,6 +1,7 @@
 const NavBarLogo = () => {
   return (
-    <div className="font-bold text-xl whitespace-nowrap bg-gradient-to-r from-[#8fcda1] to-[#5cbac4] bg-clip-text text-transparent">
+    // <div className="font-bold text-xl whitespace-nowrap bg-gradient-to-r from-[#8fcda1] to-[#5cbac4] bg-clip-text text-transparent">
+    <div className="font-bold text-xl whitespace-nowrap bg-gradient-to-r from-ucf-dark to-ucf-light bg-clip-text text-transparent">
       GROUP 22
     </div>
   );
