bf2a9ea4199dff951358ec44810415e4bed1af91
Water-Script <lancemitchj00@gmail.com>
2025-04-12 19:05:12 -0400

    Movie main movie info to component

--- frontend/src/pages/cards/[cardId]/CardDetailsPage.tsx
+++ frontend/src/pages/cards/[cardId]/CardDetailsPage.tsx
@@ -1,12 +1,12 @@
 import { useParams } from "react-router-dom";
-import { FaHeart, FaRegHeart, FaBookmark, FaRegBookmark } from "react-icons/fa";
 import ErrorPage from "../../error/ErrorPage";
+import InfoBanner from "./_components/InfoBanner";
 import { isAuthenticated } from "@/api/apiClient";
 import { useUserSettings } from "@/hooks/useMovies";
-import RatingBadge from "@/components/movie_card/RatingBadge";
-import { useInfiniteMoviesSearch, type MovieData } from "@/hooks/useMovies";
+import { useInfiniteMoviesSearch, type MovieData, type UserSettings } from "@/hooks/useMovies";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Button } from "@/components/ui/button";
+import { Info } from "lucide-react";
 
 const CardDetailsPage = () => {
   const { data: userSettings } = isAuthenticated() ? useUserSettings() : { data: [] };
@@ -17,7 +17,20 @@ const CardDetailsPage = () => {
     return <ErrorPage />;
   }
 
-  const movieUserSettings = userSettings?.find((movie) => movie.movieId === movieId);
+  const movieUserSettings = userSettings?.find((movie) => movie.movieId === movieId) ?? {
+    //* Temp for testing
+    isLiked: false,
+    isSaved: false,
+    movieId: 1,
+    rating: 5,
+  };
+
+  if (!movieUserSettings) {
+    console.error("Failed to load user data.");
+    return <ErrorPage />;
+  }
+
+  const { isLiked, isSaved, rating } = movieUserSettings;
 
   const { data, isLoading, error } = /*useInfiniteMoviesSearch(cardId) */ {
     data: {
@@ -51,61 +64,11 @@ const CardDetailsPage = () => {
   }
 
   return (
-    <div className="w-full h-fit bg-cover bg-center bg-no-repeat bg-[url(https://image.tmdb.org/t/p/original/2Nti3gYAX513wvhp8IiLL6ZDyOm.jpg)]">
-      <div className="w-full h-full bg-linear-to-r from-black/100 from-10% md:from-5% to-black/80 to-40% md:to-25%">
-        <div className="flex flex-row columns-2 p-7 pl-8 h-full w-full ">
-          <div id="poster" className="relative min-w-[200px] mr-5 aspect-2/3">
-            <Skeleton className="w-full aspect-[2/3]" />
-            <img
-              src={`https://image.tmdb.org/t/p/original/${movieData.poster_path}`}
-              alt={movieData.title}
-              className="absolute w-full object-cover inset-0 aspect-2/3"
-            />
-          </div>
-          <div className="flex flex-col">
-            <h1 className="text-white text-4xl font-extrabold mb-1">
-              {movieData.title}
-              <span className="text-gray-500 text-3xl font-semibold">
-                {` (${movieData.release_date})`}
-              </span>
-            </h1>
-            <p className="text-white">{movieData.overview}</p>
-            <div className="flex flex-col gap-5 mt-3">
-              <div className="inline-flex h-fit">
-                <span className="text-white inline-block font-semibold text-lg mr-2 align-top">
-                  Average rating:{" "}
-                </span>
-                <RatingBadge rating={movieData.vote_average} />
-              </div>
-              <div className="flex flex-col gap-2">
-                <Button>
-                  {movieUserSettings?.isLiked ? (
-                    <>
-                      <FaHeart className="text-rose-500" /> Unlike
-                    </>
-                  ) : (
-                    <>
-                      <FaRegHeart className="text-white" /> Like
-                    </>
-                  )}
-                </Button>
-                <Button>
-                  {movieUserSettings?.isSaved ? (
-                    <>
-                      <FaBookmark className="text-yellow-400" /> Unsave
-                    </>
-                  ) : (
-                    <>
-                      <FaRegBookmark className="text-white" /> Save
-                    </>
-                  )}
-                </Button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <section>
+        <InfoBanner movieData={movieData} isLiked={isLiked} isSaved={isSaved} rating={rating} />
+      </section>
+    </>
   );
 };
 
---
+++ frontend/src/pages/cards/[cardId]/_components/InfoBanner.tsx
@@ -0,0 +1,78 @@
+import { FaHeart, FaRegHeart, FaBookmark, FaRegBookmark } from "react-icons/fa";
+import { type MovieData } from "@/hooks/useMovies";
+import RatingBadge from "@/components/movie_card/RatingBadge";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+
+interface Props {
+  movieData: MovieData;
+  isLiked: boolean;
+  isSaved: boolean;
+  rating: number | null;
+}
+
+const imageUrlBase = "https://image.tmdb.org/t/p/original/";
+
+const InfoBanner: React.FunctionComponent<Props> = ({ movieData, isLiked, isSaved, rating }) => {
+  return (
+    <div
+      className={`w-full h-fit bg-cover bg-center bg-no-repeat bg-[url(${imageUrlBase}${movieData.poster_path})]`}
+    >
+      <div className="w-full h-full bg-linear-to-r from-black/100 from-10% md:from-5% to-black/80 to-40% md:to-25%">
+        <div className="flex flex-row columns-2 p-7 pl-8 h-full w-full ">
+          <div id="poster" className="relative min-w-[200px] mr-5 aspect-2/3">
+            <Skeleton className="w-full aspect-[2/3]" />
+            <img
+              src={`${imageUrlBase}${movieData.backdrop_path}`}
+              alt={movieData.title}
+              className="absolute w-full object-cover inset-0 aspect-2/3"
+            />
+          </div>
+          <div className="flex flex-col">
+            <h1 className="text-white text-4xl font-extrabold mb-1">
+              {movieData.title}
+              <span className="text-gray-500 text-3xl font-semibold">
+                {` (${movieData.release_date})`}
+              </span>
+            </h1>
+            <p className="text-white">{movieData.overview}</p>
+            <div className="flex flex-col gap-5 mt-3">
+              <div className="inline-flex h-fit">
+                <span className="text-white inline-block font-semibold text-lg mr-2 align-top">
+                  Average rating:{" "}
+                </span>
+                <RatingBadge rating={movieData.vote_average} />
+              </div>
+              <div className="flex flex-col gap-2">
+                <Button>
+                  {isLiked ? (
+                    <>
+                      <FaHeart className="text-rose-500" /> Unlike
+                    </>
+                  ) : (
+                    <>
+                      <FaRegHeart className="text-white" /> Like
+                    </>
+                  )}
+                </Button>
+                <Button>
+                  {isSaved ? (
+                    <>
+                      <FaBookmark className="text-yellow-400" /> Unsave
+                    </>
+                  ) : (
+                    <>
+                      <FaRegBookmark className="text-white" /> Save
+                    </>
+                  )}
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InfoBanner;
