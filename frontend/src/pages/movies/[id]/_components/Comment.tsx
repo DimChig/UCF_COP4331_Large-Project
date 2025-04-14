41440e39f5b315ec8b680679f9d1501dae06f93f
DimChig <dima.chigarev@gmail.com>
2025-04-13 21:14:28 -0400

    Working comment deletion

--- frontend/src/pages/movies/[id]/_components/Comment.tsx
+++ frontend/src/pages/movies/[id]/_components/Comment.tsx
@@ -5,12 +5,51 @@ import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { CommentData } from "@/hooks/useComments";
 import { FaTrash } from "react-icons/fa";
 import RatingStarsSimple from "./RatingStarsSimple";
+import { baseUrl, getAuthHeader, isAuthenticated } from "@/api/apiClient";
+import { useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import { toast } from "sonner";
+import { ImSpinner2 } from "react-icons/im";
 
 interface Props {
   comment: CommentData;
 }
 
 const Review = ({ comment }: Props) => {
+  const queryClient = useQueryClient();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const deleteComment = async (commentId: string) => {
+    if (!isAuthenticated()) return;
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(`${baseUrl}/api/comments/${commentId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: getAuthHeader(),
+        },
+      });
+
+      if (!response.ok) {
+        toast.error("Failed to delete comment", {
+          description: `API request failed with HTTP ${response.status} ${
+            response.statusText
+          }: ${JSON.parse(await response.text()).error}`,
+        });
+        return;
+      }
+
+      queryClient.invalidateQueries({ queryKey: ["comments"] });
+      toast.success("Comment deleted successfully");
+    } catch (error) {
+      toast.error("Error deleting comment:", { description: String(error) });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Card className="w-full">
       <CardHeader>
@@ -45,7 +84,14 @@ const Review = ({ comment }: Props) => {
           </div>
           {comment.isMine && (
             <div className="flex flex-row items-center gap-2">
-              <FaTrash className="w-4 h-4 cursor-pointer opacity-50 hover:opacity-100 hover:text-red-500" />
+              {isLoading ? (
+                <ImSpinner2 className="animate-spin" />
+              ) : (
+                <FaTrash
+                  onClick={() => deleteComment(comment.id)}
+                  className="w-4 h-4 cursor-pointer opacity-50 hover:opacity-100 hover:text-red-500"
+                />
+              )}
             </div>
           )}
         </div>
